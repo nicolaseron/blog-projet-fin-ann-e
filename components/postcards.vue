03ec9c34d08f134e2bbe599d0ba81909cbbefdca
nicolaseron <nicoco@live.be>
2024-02-17 16:04:17 +0100

    add card cmp into all posts

--- components/postcards.vue
+++ components/postcards.vue
@@ -1,22 +1,22 @@
 <template>
-        <a
-        href="#"
-        class="bg-blue400 text-blue100 rounded-2xl w-96 h-60 overflow-hidden p-5 border border-blue400 hover:border-blue100"
-      >
-        <h1 class="text-center mb-5">Titre de l'article</h1>
-        <div class="flex gap-5">
-          <p class="allPostsDescritpion">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Repudiandae, quisquam tenetur velit officia, deleniti esse nulla
-            iste ipsa quia cumque voluptatum ipsam. Architecto nisi corrupti
-            tempore esse doloribus magni, ab modi, eius, adipisci eum libero
-            quas vero facere repellendus nulla itaque ut error? Numquam quis,
-            beatae porro temporibus dolores architecto.
-          </p>
-          <img
-            class="w-32 h-32 rounded-xl"
-            src="/img/ales-nesetril-Im7lZjxeLhg-unsplash.jpg"
-          />
-        </div>
-      </a>
+  <a
+    href="#"
+    class="bg-blue400 text-blue100 rounded-2xl w-96 h-60 overflow-hidden p-5 border border-blue400 hover:border-blue100"
+  >
+    <h1 class="text-center mb-5">Titre de l'article</h1>
+    <div class="flex gap-5">
+      <p class="allPostsDescritpion">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae,
+        quisquam tenetur velit officia, deleniti esse nulla iste ipsa quia
+        cumque voluptatum ipsam. Architecto nisi corrupti tempore esse doloribus
+        magni, ab modi, eius, adipisci eum libero quas vero facere repellendus
+        nulla itaque ut error? Numquam quis, beatae porro temporibus dolores
+        architecto.
+      </p>
+      <img
+        class="w-32 h-32 rounded-xl"
+        src="/img/neom-eOWabmCNEdg-unsplash.jpg"
+      />
+    </div>
+  </a>
 </template>
