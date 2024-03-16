af99220cc8663a2caa08abcd3cd18790fba61985
nicolaseron <nicoco@live.be>
2024-03-16 10:36:03 +0100

    add tag to preview post

--- pages/addPost.vue
+++ pages/addPost.vue
@@ -2,46 +2,46 @@
   <main class="flex flex-col min-h-svh text-darkBlue">
     <the-header></the-header>
     <section class="bg-background flex-1 flex p-10 flex-col lg:flex-row">
-      <form @submit="sendPost" class="flex-1" ref="myForm">
+      <form @submit.prevent="sendPost" class="flex-1" ref="myForm">
         <div class="flex flex-col gap-y-5">
           <div class="flex flex-col gap-y-3">
             <label for="image"
-              >Choisissez une image
+            >Choisissez une image
               <span class="text-xs text-gray-500"
-                >(format accepté : '.png','.jpg','.jpeg','.webp')</span
+              >(format accepté : '.png','.jpg','.jpeg','.webp')</span
               >
               :
             </label>
             <input
-              type="file"
-              name="image"
-              id="image"
-              accept=".png,.jpg,.jpeg,.webp"
-              @change="addPreviewImg"
+                type="file"
+                name="image"
+                id="image"
+                accept=".png,.jpg,.jpeg,.webp"
+                @change="addPreviewImg"
             />
           </div>
           <div>
             <label for="title" class="mr-2">Titre du poste : </label>
             <input
-              v-model="title"
-              type="text"
-              name="title"
-              id="title"
-              required
-              class="custom_input"
+                v-model="title"
+                type="text"
+                name="title"
+                id="title"
+                required
+                class="custom_input"
             />
           </div>
           <div class="flex gap-x-3">
             <label for="content">Contenu du poste&nbsp;:</label>
             <textarea
-              class="bg-transparent border border-darkBlue"
-              name="content"
-              id="content"
-              v-model="content"
-              spellcheck="true"
-              required
-              cols="60"
-              rows="10"
+                class="bg-transparent border border-darkBlue"
+                name="content"
+                id="content"
+                v-model="content"
+                spellcheck="true"
+                required
+                cols="60"
+                rows="10"
             ></textarea>
           </div>
           <div class="flex flex-col lg:flex-row">
@@ -49,49 +49,49 @@
             <div class="gap-x-3 flex">
               <div>
                 <input
-                  type="checkbox"
-                  name="tech"
-                  id="tech"
-                  value="tech"
-                  v-model="techIschecked"
+                    type="checkbox"
+                    name="tech"
+                    id="tech"
+                    value="tech"
+                    v-model="techIschecked"
                 />
                 <label for="tech" class="ml-1">Tech</label>
               </div>
               <div>
                 <input
-                  type="checkbox"
-                  name="sante"
-                  id="sante"
-                  value="sante"
-                  v-model="healthIschecked"
+                    type="checkbox"
+                    name="sante"
+                    id="sante"
+                    value="sante"
+                    v-model="healthIschecked"
                 />
                 <label for="sante" class="ml-1">Santé</label>
               </div>
               <div>
                 <input
-                  type="checkbox"
-                  name="politique"
-                  id="politique"
-                  value="politique"
-                  v-model="politicIschecked"
+                    type="checkbox"
+                    name="politique"
+                    id="politique"
+                    value="politique"
+                    v-model="politicIschecked"
                 />
                 <label for="politique" class="ml-1">Politique</label>
               </div>
               <div>
                 <input
-                  type="checkbox"
-                  name="mode"
-                  id="mode"
-                  value="mode"
-                  v-model="modeIschecked"
+                    type="checkbox"
+                    name="mode"
+                    id="mode"
+                    value="mode"
+                    v-model="modeIschecked"
                 />
                 <label for="mode" class="ml-1">Mode</label>
               </div>
             </div>
           </div>
           <button
-            type="submit"
-            class="custom_btn w-fit self-center mb-5 lg:mb-0"
+              type="submit"
+              class="custom_btn w-fit self-center mb-5 lg:mb-0"
           >
             Ajouter le poste
           </button>
@@ -100,27 +100,27 @@
       <div class="flex-1 lg:ml-5">
         <h1 class="text-2xl">Rendu sur la page :</h1>
         <div
-          class="border border-darkBlue max-w-[700px] h-auto min-h-[90%] mt-5 p-5 lg:p-10 break-words"
+            class="border border-darkBlue max-w-[700px] h-auto min-h-[90%] mt-5 p-5 lg:p-10 break-words"
         >
           <div
-            class="flex gap-x-2 justify-end text-black flex-wrap gap-y-2 lg:gap-y-0"
+              class="flex gap-x-2 justify-end text-black flex-wrap gap-y-2 lg:gap-y-0"
           >
             <span v-show="techIschecked" class="bg-yellow-300 p-2 rounded-xl"
-              >Tech</span
+            >Tech</span
             >
             <span v-show="healthIschecked" class="bg-red-500 p-2 rounded-xl"
-              >Santé</span
+            >Santé</span
             >
             <span v-show="politicIschecked" class="bg-blue-400 p-2 rounded-xl"
-              >Politique</span
+            >Politique</span
             >
             <span v-show="modeIschecked" class="bg-pink-400 p-2 rounded-xl"
-              >Mode</span
+            >Mode</span
             >
           </div>
           <img
-            ref="previewImage"
-            class="max-h-[300px] w-full object-contain my-5"
+              ref="previewImage"
+              class="max-h-[300px] w-full object-contain my-5"
           />
           <h2 class="text-xl font-bold mb-8">{{ title }}</h2>
           <p class="whitespace-pre-line">{{ content }}</p>
@@ -139,6 +139,7 @@ const politicIschecked = ref(false);
 const modeIschecked = ref(false);
 const previewImage = ref(null);
 const myForm = ref(null);
+
 function addPreviewImg(e) {
   const file = e.target.files[0];
 
@@ -152,6 +153,7 @@ function addPreviewImg(e) {
     reader.readAsDataURL(file);
   }
 }
+
 async function sendPost() {
   const dataForm = new FormData(myForm.value);
   try {
--- server/api/form.post.ts
+++ server/api/form.post.ts
@@ -1,26 +1,48 @@
 import {
     S3Client,
     PutObjectCommand,
-    GetObjectCommand,
 } from "@aws-sdk/client-s3";
+
 export default defineEventHandler(async (event) => {
 
     const s3Client = new S3Client({});
+
     interface MyData {
         imgType?: string;
         imgData?: string | Buffer;
         title?: string;
         content?: string;
+        tags?: string[] | undefined
     }
 
     try {
         const body = await readMultipartFormData(event)
-        const data :MyData = {}
+        const data: MyData = {}
         if (body) {
             data.imgType = body[0].type
             data.imgData = body[0].data
             data.title = body[1].data.toString()
             data.content = body[2].data.toString()
+            data.tags = []
+            body.forEach((el) => {
+                if (!data.tags) {
+                    return;
+                }
+                switch (el.name) {
+                    case "tech" :
+                        data.tags.push(el.name);
+                        break;
+                    case "sante" :
+                        data.tags.push(el.name);
+                        break;
+                    case "politique" :
+                        data.tags.push(el.name);
+                        break;
+                    case "mode" :
+                        data.tags.push(el.name);
+                        break;
+                }
+            })
         }
         const insertPost =
             "INSERT INTO posts (id_profil, title, content) VALUES ($1, $2, $3) RETURNING id";
@@ -28,13 +50,26 @@ export default defineEventHandler(async (event) => {
 
         const idPost = insertQueryPost.rows[0].id;
 
+        const insertTags = "INSERT INTO intermediary_tags_posts VALUES ($1 , $2)"
+        const tagsArray: number[] = [];
+        if (data.tags && data.tags.length > 0) {
+            for (const el of data.tags) {
+                const idTag = await client.query(`SELECT id
+                                                  FROM tags
+                                                  WHERE name = '${el}'`);
+                tagsArray.push(idTag.rows[0].id);
+            }
+            tagsArray.sort();
+        }
+        await client.query(insertTags, [idPost, `{${tagsArray}}`]);
+
         const keyImg = `image_post_${idPost}`
         const params = {
-            Bucket : process.env.AWS_BUCKET_NAME,
-            Key : keyImg,
-            Body : data.imgData,
-            Region : process.env.AWS_REGION,
-            ContentType : data.imgType
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: keyImg,
+            Body: data.imgData,
+            Region: process.env.AWS_REGION,
+            ContentType: data.imgType
         }
         await s3Client.send(
             new PutObjectCommand({
