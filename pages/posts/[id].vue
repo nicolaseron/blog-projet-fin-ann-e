083ec37decbc9f4df940f85f421c07c8b178a79c
nicolaseron <nicoco@live.be>
2024-04-06 17:32:06 +0200

    add details post not finish

---
+++ pages/posts/[id].vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="flex flex-col min-h-svh text-darkBlue">
+    <the-header></the-header>
+    <section v-if="data">
+      <pre>{{data}}</pre>
+    </section>
+    <the-footer></the-footer>
+  </main>
+</template>
+
+<script setup lang="ts">
+
+const route = useRoute()
+const data = ref()
+
+onBeforeMount(async () => {
+  try {
+    const response = await fetch(`/api/posts/${route.params.id}`)
+    data.value = await response.json()
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données")
+  }
+})
+</script>
+
+
+<style scoped>
+
+</style>
