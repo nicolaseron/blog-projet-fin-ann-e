7e416c4b136d2edc380016aaa1af2458aca19e8f
nicolaseron <nicoco@live.be>
2024-05-06 21:45:02 +0200

    add db and change some style

--- components/PostCards.vue
+++ components/PostCards.vue
@@ -4,9 +4,9 @@
         class="bg-headerFooter text-white rounded-2xl overflow-hidden p-5 w-[300px] h-[310px] sm:w-[500px] border-2 border-headerFooter hover:bg-darkBlue"
     >
       <slot></slot>
-      <h1 class="text-center mb-5 text-2xl">{{ title }}</h1>
+      <h1 class="text-center mb-5 mt-4 text-2xl">{{ title }}</h1>
       <div class="flex gap-5 justify-between">
-        <p class="text-wrap break-words w-[50%]">{{ content }}</p>
+        <p class="text-wrap break-words w-[50%] line-clamp-5">{{ content }}</p>
         <nuxt-picture class="max-w-40 max-h-40 rounded-xl flex-1" :src="src"
                       :img-attrs="{class : 'h-full w-full object-contain'}">
         </nuxt-picture>
